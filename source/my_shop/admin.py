989f47ba9f92b5f0fc496d5d12b09f4e87963690
nurjigitberdibekov07-pixel <nurjigitberdibekov07@gmail.com>
2026-05-30 02:29:03 -0700

    Добавлены модели Categories и Products, Эарегистрированы в админке.

--- source/my_shop/admin.py
+++ source/my_shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from my_shop.models import Products, Categories
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "description", "price", "image", "category_id", "created"]
+    search_fields = ["name", "id"]
+    fields = ["name", "description", "price", "image", "category_id", "created"]
+    readonly_fields = ["created"]
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Categories, CategoriesAdmin)
--- source/my_shop/models.py
+++ source/my_shop/models.py
@@ -1,3 +1,29 @@
+from django.contrib.gis.admin import options
 from django.db import models
 
 # Create your models here.
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False ,verbose_name="Категория", unique=True)
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Описание категории")
+
+    def __str__(self):
+        return f"{self.pk} - {self.name}"
+
+class Products(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Название")
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Описание")
+    category_id = models.ForeignKey("my_shop.Categories", on_delete=models.RESTRICT, related_name="products", null=False, blank=False,)
+    created = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2, null=False, blank=False)
+    image = models.URLField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "продукты"
+        verbose_name = "продукт"
+
+
