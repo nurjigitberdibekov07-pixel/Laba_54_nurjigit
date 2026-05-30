5893534999659d62d67362030ff526eb5252c1c5
nurjigitberdibekov07-pixel <nurjigitberdibekov07@gmail.com>
2026-05-30 06:27:36 -0700

    Сделаны мелкие правки, добавлено отоброжение главной страницы.

--- source/my_shop/urls.py
+++ source/my_shop/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from my_shop.views import products_view,add_category
 
 urlpatterns = [
-
+    path('products/', products_view, name='products'),
+    # path('products/<int:pk>/', products_view, name='product'),
+    # path('categories/add/', add_category, name='add_category'),
 ]
--- source/my_shop/views.py
+++ source/my_shop/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from my_shop.models import Products, Categories
 
 # Create your views here.
+def products_view(request):
+    products = Products.objects.all()
+    context = {'products': products}
+    return render(request, "my_shop_forms/products.html", context)
+
+def add_product(request):
+    if request.method == "GET":
+        return render(request, "my_shop_form/product_view.html")
