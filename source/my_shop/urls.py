c4fe5c31efeaac84fde8c4c0efa78ee374bac431
nurjigitberdibekov07-pixel <nurjigitberdibekov07@gmail.com>
2026-05-31 23:27:52 -0700

    Реализована логика добавления категории.

--- source/my_shop/urls.py
+++ source/my_shop/urls.py
@@ -4,5 +4,5 @@ from my_shop.views import products_view,add_category
 urlpatterns = [
     path('products/', products_view, name='products'),
     # path('products/<int:pk>/', products_view, name='product'),
-    # path('categories/add/', add_category, name='add_category'),
+    path('categories/add/', add_category, name='add_category'),
 ]
--- source/my_shop/views.py
+++ source/my_shop/views.py
@@ -11,3 +11,14 @@ def products_view(request):
 def add_product(request):
     if request.method == "GET":
         return render(request, "my_shop_form/product_view.html")
+
+
+def add_category(request):
+    if request.method == "GET":
+        return render(request, "my_shop_forms/category_add.html")
+    elif request.method == "POST":
+        name = request.POST.get("name", "").strip()
+        if not name:
+            return render(request, "my_shop_forms/category_add.html", {"error": "Please enter a name"})
+        Categories.objects.create(name=name, description=request.POST.get("description", "").strip())
+        return redirect("add_category")
